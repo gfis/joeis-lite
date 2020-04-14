faabcf9740e55ab2987c2434baea0afd18cfe068
Georg Fischer <dr.Georg.Fischer@gmail.com>
2020-04-14 15:54:39 +0200

    seq4

--- internal/fischer/seq4.create.sql
+++ internal/fischer/seq4.create.sql
@@ -11,7 +11,7 @@ CREATE  TABLE            seq4
     , callcode VARCHAR(32)           -- cfsnum
     , offset   VARCHAR(16)           -- offset
     , parm1    VARCHAR(4096)
-    , parm2    VARCHAR(256)
+    , parm2    VARCHAR(1024)
     , parm3    VARCHAR(256)
     , parm4    VARCHAR(4096)          
     , parm5    VARCHAR(256)          
