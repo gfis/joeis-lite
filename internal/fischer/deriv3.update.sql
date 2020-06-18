32c9f1a33b56eca4fba1c846feea0cb9b1446798
Georg Fischer <dr.Georg.Fischer@gmail.com>
2020-06-18 10:08:55 +0200

    deriv3.update.sql

--- internal/fischer/deriv3.update.sql
+++ internal/fischer/deriv3.update.sql
@@ -3,4 +3,5 @@
 -- 2020-06-17: Georg Fischer
 --
 UPDATE seq4 SET PARM1 = '~~A005493_0 - A000110_0__1' WHERE aseqno = 'A033452';
+UPDATE seq4 SET PARM1 = '~~10*A003136_1__1 + 2'      WHERE aseqno = 'A071336';
 COMMIT;
