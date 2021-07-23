49a4658d4de8419d22b2fbfa901bbcf8a362f26c
Georg Fischer <dr.Georg.Fischer@gmail.com>
2021-07-23 19:37:57 +0200

    cofr

---
+++ internal/fischer/cofr.sql
@@ -0,0 +1,10 @@
+--  Patches for target cofr
+--  @(#) $Id$
+--  2021-07-23: Georg Fischer
+--
+DELETE FROM seq4 WHERE aseqno    IN (SELECT aseqno FROM joeis);
+COMMIT;
+DELETE FROM seq4 WHERE parm1 NOT IN (SELECT aseqno FROM joeis WHERE superclass = 'DecimalExpansionSequence');
+UPDATE seq4 SET parm1 = 'A010774' WHERE aseqno = 'A103922';
+DELETE FROM seq4 WHERE aseqno = 'A053002';
+COMMIT;
