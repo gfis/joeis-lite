3078649d02bbaefc3f700cc271be7758d018d19b
Georg Fischer <dr.Georg.Fischer@gmail.com>
2021-08-10 13:03:16 +0200

    getcr?.jpat

--- internal/fischer/decsolv.sql
+++ internal/fischer/decsolv.sql
@@ -2,4 +2,5 @@
 -- 2021-08-01, Georg Fischer
 --
 UPDATE seq4 SET parm2 = '.inverse()' WHERE aseqno IN ('A196503');
+UPDATE seq4 SET parm3='0.0605', parm4='0.0630', offset=0 WHERE aseqno ='A260632';
 COMMIT;
