e34fa46d71480024aecaa71868128c7051c9552b
Georg Fischer <dr.Georg.Fischer@gmail.com>
2021-08-12 15:29:16 +0200

    bref+noncomp

--- internal/fischer/noncomp.sql
+++ internal/fischer/noncomp.sql
@@ -1,6 +1,7 @@
---  Patches for callcode sciconst
+--  Patches for callcode noncomp
 --  @(#) $Id$
+--  2021-08-12: always generate with select EX=xx
 --  2021-07-24: Georg Fischer
 --
-UPDATE seq4 s SET parm1 = (SELECT d.data FROM asdata d WHERE s.aseqno = d.aseqno);
+DELETE FROM seq4 WHERE aseqno IN (SELECT aseqno FROM joeis WHERE aseqno NOT IN ('A308681'));
 COMMIT;
