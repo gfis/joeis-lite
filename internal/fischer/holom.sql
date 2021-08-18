d4a5b85996f7c23c05419b8607dc62f822602087
Georg Fischer <dr.Georg.Fischer@gmail.com>
2021-08-18 17:32:28 +0200

    linmangf

--- internal/fischer/holom.sql
+++ internal/fischer/holom.sql
@@ -1,6 +1,11 @@
 -- Patches for holom
 -- @(#) $Id$
+-- 2021-08-18: holom, patch inits[0]
 -- 2021-06-06, Georg Fischer; RH=75; RM oo 50.
 
-UPDATE seq4 SET offset=0 WHERE aseqno IN ('A121989','A122022','A122050','A122752','A288954');
+-- UPDATE seq4 SET offset=0 WHERE aseqno IN ('A121989','A122022','A122050','A122752','A288954');
+UPDATE seq4 SET parm2 = '[1,' || SUBSTR(parm2, 2, LENGTH(parm2) - 1) WHERE aseqno
+  IN ('A054886','A074764','A082398','A085801','A155137','A334930');
+UPDATE seq4 SET parm2 = '[0,' || SUBSTR(parm2, 2, LENGTH(parm2) - 1) WHERE aseqno
+  IN ('A093611','A116547','A220360');
 COMMIT;
