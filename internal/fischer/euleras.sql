77e07476a91a6e2e2b7429b53cea86e939d1b746
Georg Fischer <dr.Georg.Fischer@gmail.com>
2021-11-24 23:09:27 +0100

    euleras, weighi, prodsi2

--- internal/fischer/euleras.sql
+++ internal/fischer/euleras.sql
@@ -1,6 +1,8 @@
 --  Patches for CC=euleras
 --  @(#) $Id$
+--  2021-11-24: individual DELETE
 --  2020-12-03: Georg Fischer
 --
 DELETE FROM seq4 WHERE parm2 NOT IN (SELECT aseqno FROM joeis WHERE superclass IS NOT NULL);
+DELETE FROM seq4 WHERE aseqno IN ('A029726','A134964','A137917','A173239','A174065','A328550');
 COMMIT;
