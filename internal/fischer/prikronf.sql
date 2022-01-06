1a7b46195c0f04b3be87f03382b45ac9904ba076
Georg Fischer <dr.Georg.Fischer@gmail.com>
2022-01-06 17:46:25 +0100

    1740 pricongr

--- internal/fischer/prikronf.sql
+++ internal/fischer/prikronf.sql
@@ -1,9 +1,8 @@
 --  Patches for CC=pricongr
 --  @(#) $Id$
---  2022-01-03: Georg Fischer
+--  2022-01-06: Georg Fischer
 --
 UPDATE seq4 SET parm1 = 15 WHERE aseqno IN ('A033212','A191018');
 UPDATE seq4 SET parm1 = 22 WHERE aseqno IN ('A191020');
--- UPDATE seq4 SET parm2 = -1 WHERE aseqno IN ('A191060','A191060','A191060','A191060','A191060','A191060','A191060','A191060');
-DELETE FROM seq4   WHERE aseqno IN ('A000000');
+DELETE FROM seq4           WHERE aseqno IN ('A191018','A191019','A191061');
 COMMIT;
