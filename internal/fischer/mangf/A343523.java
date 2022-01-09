d0f9506963299bc6dc3156105a064c162eee2cd1
Georg Fischer <dr.Georg.Fischer@gmail.com>
2022-01-09 15:37:47 +0100

    squaredig4

--- internal/fischer/mangf/A343523.java
+++ internal/fischer/mangf/A343523.java
@@ -1,5 +1,5 @@
 package irvine.oeis.a343;
-// manually 2021-06-21
+// manually 2022-01-06
 
 import irvine.math.z.Binomial;
 import irvine.math.z.Z;
@@ -9,11 +9,24 @@ import irvine.oeis.MemorySequence;
  * A343523 a(0) = 1; a(n) = 2 * Sum_{k=1..n} binomial(n,k) * a(k-1).
  * @author Georg Fischer
  */
-
 public class A343523 extends MemorySequence {
-    
+
+  protected int mMult;
   protected int mN = -1; // current index
-  
+
+  /** Construct the sequence. */
+  public A343523() {
+    this(2);
+  }
+
+  /**
+   * Generic constructor with parameters
+   * @param mult 
+   */
+  public A343523(final int mult) {
+    mMult = mult;
+  }
+
   @Override
   public Z computeNext() {
     ++mN;
@@ -24,6 +37,6 @@ public class A343523 extends MemorySequence {
     for (int k = 1; k <= mN; ++k) {
       sum = sum.add(Binomial.binomial(mN, k).multiply(a(k - 1)));
     }
-    return sum.multiply2();
+    return sum.multiply(mMult);
   }
 }
---
+++ internal/fischer/squaredig4.pl
@@ -0,0 +1,51 @@
+#!perl
+
+# Extract parameters for SquareDigitSequence, A053880 ff.
+# 2022-01-09, Georg Fischer: copied from invbinom.pl
+#
+#:# Usage:
+#:#   grep -E "^digits +\{[0-9]" $(COMMON)/joeis_names.txt \
+#:#   | perl squaredig4.pl [-d debug] $(COMMON)/joeis_names.txt > output.seq4
+#:#        -d  debugging level (0=none (default), 1=some, 2=more)
+#--------------------------------------------------------
+use strict;
+use integer;
+use warnings;
+
+my $line = "";
+my ($aseqno, $superclass, $callcode, $name, @rest);
+my ($rseqno);
+my $debug   = 0;
+while (scalar(@ARGV) > 0 and ($ARGV[0] =~ m{\A[\-\+]})) {
+    my $opt = shift(@ARGV);
+    if (0) {
+    } elsif ($opt   =~ m{\-d}  ) {
+        $debug      = shift(@ARGV);
+    } else {
+        die "invalid option \"$opt\"\n";
+    }
+} # while $opt
+
+my $offset = 1;
+$callcode = "squaredig4";
+# while (<DATA>) { # from joeis_names.txt
+while (<>) { # from joeis_names.txt
+    s/\s+\Z//; # chompr
+    $line = $_;
+    ($aseqno, $superclass, $name, @rest) = split(/\t/, $line);
+    next if $superclass ne "null";
+    if (0) {
+    } elsif ($name =~ m{\A(Numbers \w such that \w\^2 contains only digits|Squares composed of digits)}) {
+        my $mode = (substr($1, 0, 4) eq "Numb") ? 4 : 5;
+        $name =~ m{digits\D+([\d\, ]+)};
+        my $subset = $1;
+        $subset =~ s{ }{}g;
+        my $zeroes = ($name =~ m{not ending with zero]) ? 1 : 0;
+        print join("\t", $aseqno, "$callcode", $offset, 10, $mode, $subset, $zeroes, $name) . "\n";
+    }
+} # while <>
+__DATA__
+A053880	null	Numbers k such that k^2 contains only digits {1,2,4}.	nonn,base,changed,synth	1..28	nyi
+A053881	null	Squares composed of digits {1,2,4}.	nonn,base,synth	1..20	nyi
+A053882	null	Numbers k such that k^2 contains only digits {1,2,6}.	nonn,base,more,changed,synth	1..17	nyi
+A053883	null	Squares composed of digits {1,2,6}.	nonn,base,synth	1..15	nyi
