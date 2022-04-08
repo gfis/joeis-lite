7e81f4bf1c7e411fbae2b1f9adaf44ebdf957d99
Georg Fischer <dr.Georg.Fischer@gmail.com>
2022-04-08 23:04:26 +0200

    anabuild

--- internal/fischer/trimirror.sql
+++ internal/fischer/trimirror.sql
@@ -1,5 +1,7 @@
 -- trimirror.sql: patches for trimirror
+-- @(#) $Id$
 -- 2021-10-31, Georg Fischer
 --
 DELETE FROM seq4 WHERE parm1 NOT IN (SELECT aseqno FROM joeis);
+DELETE FROM seq4 WHERE aseqno IN ('A095715','A163235','A165194');
 COMMIT;
