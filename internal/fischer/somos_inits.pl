8d594657cafb829a08b34315d2bb6090cb989a56
Georg Fischer <dr.Georg.Fischer@gmail.com>
2022-06-03 23:31:34 +0200

    60 x somos3

--- internal/fischer/somos_inits.pl
+++ internal/fischer/somos_inits.pl
@@ -24,22 +24,22 @@ while (<>) {
     next if $line !~ m{\AA\d+};
     my ($aseqno, $callcode, @parms) = split(/\t/, $line);
     my $ninits    = $parms[1]; # start with order
-    my $init_list = $parms[2];
+    my $init_list = $parms[3];
     my @inits = split(/\, */, $init_list);
     my $in = 0;
-    while ($inits[$in ++] == 0) {
+    while ($inits[$in ++] == 0 && $in < scalar(@inits)) {
         $ninits ++;
     }
     if ($ninits >= scalar(@inits) - 1) {
         print STDERR "# $aseqno needs more than $ninits initial terms, has " . scalar(@inits) . ": ". join(",", @inits) . "\n";
     } else {
         my @terms = splice(@inits, 0, $ninits);
-        $parms[2] = join(",", @terms);
+        $parms[3] = join(",", @terms);
         print join("\t", $aseqno, $callcode, @parms) . "\n";
     }
 } # while 
 __DATA__
-A165905	somos4	0	4	0,1,2,1,1,1,12312	1	1	
-A162546	somos4	0	4	0,0,1,2,1,1,1,12312	36	-68	Somos-4 variant:
-A171422	somos4	0	4	0,1,2,1,1,21321	-4	-5	Somos-4 sequence
-A174017	somos4	0	4	0,1,2,1,12312	1	1	Somos-4 sequence
+A165905	somos4	0	4	0	0,1,2,1,1,1,12312	1	1	
+A162546	somos4	0	4	0	0,0,1,2,1,1,1,12312	36	-68	Somos-4 variant:
+A171422	somos4	0	4	0	0,1,2,1,1,21321	-4	-5	Somos-4 sequence
+A174017	somos4	0	4	0	0,1,2,1,12312	1	1	Somos-4 sequence
