fbca409cfae64bb645866b7d18d7b59dbcbff779
Georg Fischer <dr.Georg.Fischer@gmail.com>
2022-11-10 08:53:08 +0100

    mangf

---
+++ internal/fischer/mangf/A143622.java
@@ -0,0 +1,16 @@
+package irvine.oeis.a143;
+
+import irvine.math.z.Z;
+import irvine.oeis.recur.PeriodicSequence;
+
+/**
+ * A143622 a(n) = (-1)^binomial(n,8): Periodic sequence 1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,... .
+ * @author Georg Fischer
+ */
+public class A143622 extends PeriodicSequence {
+
+  /** Construct the sequence. */
+  public A143622() {
+    super(1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1);
+  }
+}
