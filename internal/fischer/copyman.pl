3b790e114e1f8a9c396a503d4310c978b1179cca
Georg Fischer <dr.Georg.Fischer@gmail.com>
2023-01-20 23:24:42 +0100

    20 x A212551 etc.

--- internal/fischer/copyman.pl
+++ internal/fischer/copyman.pl
@@ -12,19 +12,22 @@ my %dirs;
 my $tarroot = "../../src/irvine/oeis";
 foreach my $name(glob("manual/*.java")) {
     $name =~ m{\/(A\d+)};
-    my $aseqno = $1;
-    my $tarpath = "$tarroot/" . lc(substr($aseqno, 0, 4));
+    my $aseqno  = $1;
+    my $tarpack = lc(substr($aseqno, 0, 4));
+    my $tarpath = "$tarroot/$tarpack";
     if (! defined($dirs{$tarpath}) or ! -d "$tarpath") { # tarpath not yet readable
         $dirs{$tarpath} = 1;
         mkdir($tarpath);
     }
     open(SRC, "<", $name) || die "cannot read $name\n";
     my $buffer = <SRC>;
+    $buffer =~ m{\Apackage irvine.oeis.(a\d+)};
+    my $srcpack = $1 || "undef";
     close(SRC);
     open(TAR, ">", "$tarpath/$aseqno.java") || die "cannot write $tarpath/$aseqno.java\n";
     print TAR $buffer;
     close(TAR);
     print "$aseqno\n";
-    print STDERR "copied manual/$aseqno.java -> $tarpath/$aseqno.java\n";
+    print STDERR "copied manual/$aseqno.java -> $tarpath/$aseqno.java" . (($srcpack ne $tarpack) ? " ** wrong source package $srcpack **" : "") . "\n";
 }
 __DATA__
