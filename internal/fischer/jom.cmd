c97eabf5a9580ef801275b8f1adb729b62f1b115
Georg Fischer <dr.Georg.Fischer@gmail.com>
2023-04-30 20:44:33 +0200

    65 x nconject

--- internal/fischer/jom.cmd
+++ internal/fischer/jom.cmd
@@ -2,4 +2,5 @@
 set parm=%1
 set subdir=a%parm:~1,3%
 cp -v C:/Users/User/work/gits/joeis/src/irvine/oeis/%subdir%/%parm%.java manual
+cmd /c start https://oeis.org/%parm%
 uedit64 manual\%parm%.java
