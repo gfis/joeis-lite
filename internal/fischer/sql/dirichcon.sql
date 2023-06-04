0ae82f4698866364a7052304ca4ebf99845d46d5
Georg Fischer <dr.Georg.Fischer@gmail.com>
2023-06-04 22:21:13 +0200

    23 dirichcon.pass.log

--- internal/fischer/sql/dirichcon.sql
+++ internal/fischer/sql/dirichcon.sql
@@ -1,5 +1,6 @@
 -- dirichcon.sql
 -- @(#) $Id$
+-- 2023-06-04: patch dirichinv0
 -- 2023-04-24, Georg Fischer
 
 -- A349612  dirichcon2  1   new A342001()   1   new A325126()   1   
@@ -9,4 +10,6 @@ DELETE FROM seq4 WHERE SUBSTR(parm3, 5, 7) NOT IN (select aseqno FROM joeis) AND
 COMMIT;
 UPDATE seq4 s SET parm2 = (SELECT offset1 FROM asinfo i WHERE i.aseqno = SUBSTR(s.parm1, 5, 7));
 UPDATE seq4 s SET parm4 = (SELECT offset1 FROM asinfo i WHERE i.aseqno = SUBSTR(s.parm3, 5, 7) AND s.callcode LIKE 'dirichc%');
+COMMIT;
+UPDATE seq4 s set s.CALLCODE = 'dirichinv0' WHERE s.CALLCODE = 'dirichinv' AND s.parm2 = 0;
 COMMIT;
