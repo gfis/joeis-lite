c199be96429d41a3defcc8ea497c67d43cffd848
Georg Fischer <dr.Georg.Fischer@gmail.com>
2024-01-21 23:54:41 +0100

    87 man.pass.log

--- internal/fischer/gen_seq4.pl
+++ internal/fischer/gen_seq4.pl
@@ -202,8 +202,9 @@ while (<>) { # read inputfile
             $parm =~ s{FA\(}           {MemoryFactorial.SINGLETON.factorial\(}g;
             $parm =~ s{FD\(}           {MemoryFactorial.SINGLETON.doubleFactorial\(}g;
             $parm =~ s{FI\(}           {Fibonacci.fibonacci\(}g;
-            $parm =~ s{MU\(}           {Mobius.mobius\(}g;
+            $parm =~ s{LU\(}           {Fibonacci.lucas\(}g;
             $parm =~ s{JF\(}           {Jaguar.factor(}g;
+            $parm =~ s{MU\(}           {Mobius.mobius\(}g;
             $parm =~ s{PM\(}           {Puma.primeZ\(}g;
             $parm =~ s{PR\(}           {Integers.SINGLETON.product\(}g;
             $parm =~ s{PT\(}           {IntegerPartition.partitions\(}g;
