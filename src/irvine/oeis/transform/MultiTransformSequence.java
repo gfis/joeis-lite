24b2d3f2f4114cd352033cd7b5d3df44502000ac
Georg Fischer <dr.Georg.Fischer@gmail.com>
2024-04-14 09:16:04 +0200

    s(0)

--- src/irvine/oeis/transform/MultiTransformSequence.java
+++ src/irvine/oeis/transform/MultiTransformSequence.java
@@ -40,8 +40,8 @@ public class MultiTransformSequence extends AbstractSequence {
   private final AbstractSequence[] mSeqs; // underlying source sequences 
   private final int mOffset; // offset of target sequence
   private final int [] mOffsets; // offsets of mSeqs
-  private final int mSeqNo; // number of underlying sequences s[i] + 1
-  public final Z[] s; // terms of of the source sequences
+  private final int mSeqNo; // number of underlying sequences s(i) + 1
+  private final Z[] mTerms; // terms of of the source sequences
   private final ArrayList<Z> mA; // existing target sequence elements: a(n-1), a(n-k) etc.
   private final Z[] mInits; // initial terms
   private final int mInitNo; // mInits.length
@@ -50,9 +50,9 @@ public class MultiTransformSequence extends AbstractSequence {
   private static final boolean VERBOSE = "true".equals(System.getProperty("oeis.verbose"));
 
   /** seq1 + seq2 */
-  public static final MultiFunction<MultiTransformSequence, Integer, Z> ADD = (self, n) -> self.s[0].add(self.s[1]);
+  public static final MultiFunction<MultiTransformSequence, Integer, Z> ADD = (self, n) -> self.s(0).add(self.s(1));
   /** seq1 - seq2 */
-  public static final MultiFunction<MultiTransformSequence, Integer, Z> SUB = (self, n) -> self.s[0].subtract(self.s[1]);
+  public static final MultiFunction<MultiTransformSequence, Integer, Z> SUB = (self, n) -> self.s(0).subtract(self.s(1));
 
   /**
    * An indicator that instead of the current term of a sequence <code>s[i]</code>,
@@ -70,8 +70,8 @@ public class MultiTransformSequence extends AbstractSequence {
    * @param seqs list underlying source sequences seq0, seq1, seq2 and so on
    *
    * A typical pattern for the call is:
-   * <code>super(1, (self, n) -> f(n, self.s[0], self.s[1], self.s[2]), "1", new A999990(), new A999991(), new A999992())</code>
-   * The terms of the source sequences in the lambda expression are accessed by self.s[1], self.s[2] and so on.
+   * <code>super(1, (self, n) -> f(n, self.s(0), self.s(1), self.s(2)), "1", new A999990(), new A999991(), new A999992())</code>
+   * The terms of the source sequences in the lambda expression are accessed by self.s(1), self.s(2) and so on.
    * <p />
    * If there are some initial terms for a(n), they are exhausted first, resulting in a target offset n.
    * When a source sequence has an offset &gt; n, an appropriate number of initial target terms must be specified.
@@ -104,14 +104,14 @@ public class MultiTransformSequence extends AbstractSequence {
     mSeqNo = seqs.length;
     mSeqs = new AbstractSequence[mSeqNo];
     mOffsets = new int[mSeqNo];
-    s = new Z[mSeqNo];
+    mTerms = new Z[mSeqNo];
     for (int ix = 0; ix < mSeqNo; ++ix) {
       AbstractSequence seq = seqs[ix];
       mSeqs[ix] = seq;
       if (seq != PREVIOUS) {
         mOffsets[ix] = seq.getOffset();
       }
-      s[ix] = Z.ZERO;
+      mTerms[ix] = Z.ZERO;
     }
     mIn = -1;
     mInits = (initTerms.isEmpty() || "[]".equals(initTerms)) ? new Z[0] : ZUtils.toZ(initTerms);
@@ -123,8 +123,8 @@ public class MultiTransformSequence extends AbstractSequence {
   }
 
   /**
-   * Convenience call for existing target sequence elements.
-   * @param n index
+   * Retrieve and existing target sequence element.
+   * @param n index of target sequence
    * @return a(n)
    */
   public Z a(final int n) {
@@ -132,8 +132,17 @@ public class MultiTransformSequence extends AbstractSequence {
   }
 
   /**
+   * Retrieve a source sequence element.
+   * @param ix number of source sequence
+   * @return <code>seqs[ix].a(n)</code>
+   */
+  public Z s(final int ix) {
+    return mTerms[ix];
+  }
+
+  /**
    * Align a source sequence mSeqs[ix] (that is call <code>mSeqs[ix].next()</code>)
-   * until <code>s[ix]</code> contains mSeqs[].a(n), and shift into the <code>PREVIOUS</code>
+   * until <code>mTerms[ix]</code> contains mSeqs[].a(n), and shift into the <code>PREVIOUS</code>
    * terms appropriately.
    * @param ix number of the source sequence
    * @param targetN desired index
@@ -148,14 +157,14 @@ public class MultiTransformSequence extends AbstractSequence {
     int soff = mOffsets[ix];
     while (soff < targetN) {
       for (int i = hi - 1; i > lo; --i) {
-        s[i] = s[i - 1];
+        mTerms[i] = mTerms[i - 1];
       }
-      s[lo] = mSeqs[ix].next();
+      mTerms[lo] = mSeqs[ix].next();
       ++soff;
     }
     if (VERBOSE) {
       System.out.println("# alignSourceSequence(" + ix + ", " + targetN + ") -> mN=" + mN + ", hi=" + hi + ", mOffsets[ix]=" + mOffsets[ix]
-          + ", s[ix]=" + s[ix]);
+          + ", mTerms[ix]=" + mTerms[ix]);
     }
     return hi;
   }
@@ -174,9 +183,9 @@ public class MultiTransformSequence extends AbstractSequence {
       }
       for (int ix = mSeqNo - 1; ix >= 0; --ix) { // shift into previous source elements
         if (mSeqs[ix] == PREVIOUS) {
-          s[ix] = s[ix - 1];
+          mTerms[ix] = mTerms[ix - 1];
         } else {
-          s[ix] = mSeqs[ix].next();
+          mTerms[ix] = mSeqs[ix].next();
         }
       }
     }
