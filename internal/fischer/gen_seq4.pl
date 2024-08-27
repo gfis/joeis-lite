fb7815069b6bda21a1c6022cbd491dc87d2c66ad
Georg Fischer <dr.Georg.Fischer@gmail.com>
2024-08-27 22:40:22 +0200

    58 2024-08-27.ack.pass.log

--- internal/fischer/gen_seq4.pl
+++ internal/fischer/gen_seq4.pl
@@ -643,6 +643,7 @@ sub extract_imports { # look for Annnnnnn, ZUtils. StringUtils. CR. etc.
     if ($line =~ m{\WMorphismFixedPointSequence}   ) { $imports{"irvine.oeis.base.MorphismFixedPointSequence"  } = $itype; }
     if ($line =~ m{\WMultiplicativeSequence}       ) { $imports{"irvine.oeis.MultiplicativeSequence"           } = $itype; }
     if ($line =~ m{\WMultiTransformSequence\(}     ) { $imports{"irvine.oeis.transform.MultiTransformSequence"}  = $itype; }
+    if ($line =~ m{\WObverseConvolutionSequence}   ) { $imports{"irvine.oeis.ObverseConvolutionSequence"}        = $itype; }
     if ($line =~ m{\WPaddingSequence}              ) { $imports{"irvine.oeis.recur.PaddingSequence" }            = $itype; }
     if ($line =~ m{\WPair}                         ) { $imports{"irvine.util.Pair" }                             = $itype; }
     if ($line =~ m{\WPartialSumSequence}           ) { $imports{"irvine.oeis.PartialSumSequence"}                = $itype; }
--- internal/fischer/h.cmd
+++ internal/fischer/h.cmd
@@ -1,2 +1,2 @@
-echo %1 %2 %3
-make runholo OFF=%1 MATRIX=%2 INIT=%3
+echo %1 %2 %3 %4 %5
+make runholo OFF=%1 MATRIX=%2 INIT=%3 DIST=%4 GFTYPE=%5
