c6b35515e747b3e1c86467bd03c568ce0e468313
Georg Fischer <dr.Georg.Fischer@gmail.com>
2025-01-06 13:01:56 +0100

    PRF with Z ok?

--- src/irvine/oeis/PolynomialFieldSequence.java
+++ src/irvine/oeis/PolynomialFieldSequence.java
@@ -17,12 +17,17 @@ import irvine.oeis.AbstractSequence;
 public class PolynomialFieldSequence extends AbstractSequence {
 
   protected static int sDebug = 0;
+  public static final int OGF = 0;
+  public static final int EGF = 1;
   private static final PolynomialRingField<Z> RING = new PolynomialRingField<>(IntegerField.SINGLETON);
   private final Polynomial<Z>[] mPolys; // Polynomials referenced in the postfix string as "p0" (the initial value), "p1", "p2" and so on.
   private final String[] mPostfix; // list of operands and operators
+  private int mDist; // addititional degree
+  private int mGfType; // type of the generating function: 0 = ordinary, 1 = exponential
   private int mN; // index of the next sequence element to be computed
   private Polynomial<Z> mA; // the generating function A(x)
   private final Polynomial<Z>[] mStack; // stack where the final expression is computed
+  private Z mFactorial;
 
   /**
    * Compute successive coefficients for a generating function A(x) defined by a condition that A(x) must satisfy.
@@ -35,7 +40,26 @@ public class PolynomialFieldSequence extends AbstractSequence {
    */
   @SuppressWarnings("unchecked")
   public PolynomialFieldSequence(final int offset, final String polys, final String postfix) {
+    this(offset, polys, postfix, 0, OGF);
+  }
+
+  /**
+   * Compute successive coefficients for a generating function A(x) defined by a condition that A(x) must satisfy.
+   * @param offset first index
+   * @param polys array of polynomials, the coefficients of <code>x^i, i=0..m</code>
+   * are given as comma-separated lists, enclosed in square brackets, for example "[[0],[0,1,2],[17,0,18]]"
+   * @param postfix the equation with operands and operators in postfix polish notation, separated by the first character in
+   * the string, for example <code>A(x) = 1 + x*A(x*A(x))^2</code> -&gt; <code>;1;x;x;ax;*;A(;2;^;*;+</code>,
+   * with "ax" for <code>A(x)</code> and "A(" for substitution.
+   * @param dist additional degree
+   * @param gfType type of the generating function: 0 = ordinary, 1 = exponential
+   */
+  @SuppressWarnings("unchecked")
+  public PolynomialFieldSequence(final int offset, final String polys, final String postfix, final int dist, final int gfType) {
     super(offset);
+    mDist = dist;
+    mGfType = gfType;
+    mFactorial = Z.ONE;
     String post = trimQuotes(postfix);
     mPostfix = post.substring(1).split(Pattern.quote(post.substring(0, 1)));
     if (sDebug >= 1) {
@@ -54,7 +78,7 @@ public class PolynomialFieldSequence extends AbstractSequence {
         System.out.println("# polarr[" + k + "]=" + polarr[k]);
       }
     } // for k
-    mPolys = new Polynomial[mPolyList.size()]; 
+    mPolys = new Polynomial[mPolyList.size()];
     mStack = new Polynomial[mPostfix.length]; // <- how to avoid the 2 "unchecked" warnings?
     for (int k = 0; k < mPolyList.size(); ++k) {
       mPolys[k] = Polynomial.create(mPolyList.get(k));
@@ -83,7 +107,23 @@ public class PolynomialFieldSequence extends AbstractSequence {
     }
     return str.substring(start, behind).replaceAll(" ", "");
   }
-    
+
+  /**
+   * Print the contents of the stack in readable form.
+   * @param pfix current postfix element
+   * @param top index of top stack element
+   * @param str call point
+   */
+  protected void debugStack(final String pfix, int top, final String str) {
+    System.out.print("# pfix=" + pfix + " \t");
+    for (int is = 0; is <= top + 1 ; ++is) {
+      if (mStack[is] != null) {
+        System.out.print (" [" + (is == top ? "top" : String.valueOf(is)) + "]" + mStack[is].toString());
+      }
+    }
+    System.out.println("\t" + str);
+  }
+
   @Override
   public Z next() {
     ++mN;
@@ -96,8 +136,8 @@ public class PolynomialFieldSequence extends AbstractSequence {
       while (ifix < mPostfix.length) { // scan over the operaands and operators
         String pfix = mPostfix[ifix++];
         if (sDebug >= 2 && top >= 0) {
-          System.out.println("# pfix=" + pfix + ",  \t mStack[" + top + "]=" + mStack[top] + "\t before");
-        } 
+          debugStack(pfix, top, "before");
+        }
         switch (pfix.charAt(0)) {
           default: // should not occur
             throw new RuntimeException("invalid postfix code " + pfix);
@@ -123,7 +163,7 @@ public class PolynomialFieldSequence extends AbstractSequence {
             mStack[top] = RING.diff(mStack[top]);
             break;
           case 's': // "sub", replace the current top element by a substitution
-            mStack[top] = RING.substitute(mA, mStack[top], mN);
+            mStack[top] = RING.substitute(mA, mStack[top], mN + mDist);
             break;
           case '+':
             --top;
@@ -135,26 +175,26 @@ public class PolynomialFieldSequence extends AbstractSequence {
             break;
           case '*':
             --top;
-            mStack[top] = RING.multiply(mStack[top], mStack[top + 1], mN);
+            mStack[top] = RING.multiply(mStack[top], mStack[top + 1], mN + mDist);
             break;
           case '/':
             --top;
-            mStack[top] = RING.series(mStack[top], mStack[top + 1], mN);
+            mStack[top] = RING.series(mStack[top], mStack[top + 1], mN + mDist);
           case '<': // "<(\d+)" = shift x -> x^$1 (may be negative)
             mStack[top] = RING.shift(mStack[top], (pfix.length() <= 1) ? 1 : Integer.parseInt(pfix.substring(1)));
             break;
           case '^': // P, m -> P^m, or "^(\d+)" P -> P^$1
             if (pfix.length() == 1) {
               --top;
-              mStack[top] = RING.pow(mStack[top], Long.valueOf(mStack[top + 1].toString()), mN);
+              mStack[top] = RING.pow(mStack[top], Long.valueOf(mStack[top + 1].toString()), mN + mDist);
             } else {
-              mStack[top] = RING.pow(mStack[top], Long.parseLong(pfix.substring(1)), mN);
+              mStack[top] = RING.pow(mStack[top], Long.parseLong(pfix.substring(1)), mN + mDist);
             }
             break;
         } // switch
         if (sDebug >= 3) {
-          System.out.println("#      " + "  " + "   \t       [" + top + "]=" + mStack[top] + "\t after");
-        } 
+          debugStack(pfix, top, "after");
+        }
       } // while
     } catch (Exception exc) { // will never be reached with a proper generator
       System.err.println("# unexpected exception: " + exc.getMessage());
@@ -162,7 +202,14 @@ public class PolynomialFieldSequence extends AbstractSequence {
     }
     // mTop should be 0 here
     mA = mStack[top];
-    return mA.coeff(mN);
+    Z result = mA.coeff(mN);
+    if (mGfType == EGF) {
+      if (mN > 0) {
+        mFactorial = mFactorial.multiply(mN);
+      }
+      result = result.multiply(mFactorial);
+    }
+    return result;
   } // next
 
   /**
@@ -171,17 +218,21 @@ public class PolynomialFieldSequence extends AbstractSequence {
    * <ul>
    * <li>-b print in b-file format instead of comma separated list (default false)</li>
    * <li>-d level debugging level (default 0=none), 1=some, 2=more</li>
+   * <li>-i additional degree (default 0)</li>
    * <li>-n numTerms number of terms to be computed (default: 16)</li>
    * <li>-o offset, first index (default 0)</li>
-   * <li>-p polyList list of vectors for polynomials in x (default [[0]])</li>
+   * <li>-p polyList, list of vectors for polynomials in x (default [[0]])</li>
+   * <li>-t type of the generating function: 0 = ordinary (default), 1 = exponential</li>
    * <li>-x expression in postfix notation</li>
    * </ul>
    */
   public static void main(String[] args) {
     boolean bfile = false;
-    int debug = 0;
-    int numTerms = 16;
-    int offset  = 0;
+    int debug     = 0;
+    int dist      = 0;
+    int gfType    = 0;
+    int numTerms  = 16;
+    int offset    = 0;
     String polyList  = "[[0]]";
     String postfix = null;
     int iarg = 0;
@@ -193,12 +244,16 @@ public class PolynomialFieldSequence extends AbstractSequence {
           bfile    = true;
         } else if (opt.equals    ("-d")     ) {
           debug    = Integer.parseInt(args[iarg ++]);
+        } else if (opt.equals    ("-i")     ) {
+          dist     = Integer.parseInt(args[iarg ++]);
         } else if (opt.equals    ("-n")     ) {
           numTerms = Integer.parseInt(args[iarg ++]);
         } else if (opt.equals    ("-o")     ) {
           offset   = Integer.parseInt(args[iarg ++]);
         } else if (opt.equals    ("-p")     ) {
           polyList = args[iarg ++];
+        } else if (opt.equals    ("-t")     ) {
+          gfType   = Integer.parseInt(args[iarg ++]);
         } else if (opt.equals    ("-x")     ) {
           postfix  = args[iarg ++];
         } else {
@@ -210,7 +265,7 @@ public class PolynomialFieldSequence extends AbstractSequence {
 
     if (postfix != null) {
       PolynomialFieldSequence.sDebug = debug;
-      PolynomialFieldSequence prs = new PolynomialFieldSequence(offset, polyList, postfix);
+      PolynomialFieldSequence prs = new PolynomialFieldSequence(offset, polyList, postfix, dist, gfType);
       int ind = offset - 1;
       boolean busy = true;
       while (--numTerms >= 0) {
@@ -225,7 +280,7 @@ public class PolynomialFieldSequence extends AbstractSequence {
       System.out.println();
     } else {
       System.out.println("Usage: java -cp joeis.jar irvine.oeis.PolynomialFieldSequence"
-          + " [-b] [-d mode] [-n noterms] [-o offset] [-p polys] -x postfix");
+          + " [-b] [-d mode] [-i dist] [-n noterms] [-o offset] [-p polys] [-t gftype] -x postfix");
     }
   } // main
 }
