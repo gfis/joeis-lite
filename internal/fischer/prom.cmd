fb7e59d2cba3cc31d2a62e62af8f503dcbf32e54
Georg Fischer <dr.georg.fischer@gmail.com>
2025-04-22 22:35:23 +0200

    123 2025-04-22.ack.pass.log

--- internal/fischer/prom.cmd
+++ internal/fischer/prom.cmd
@@ -1,7 +1,7 @@
 @echo off
 set parm=%1
 set subdir=a%parm:~1,3%
-cp -v C:/Users/User/work/gits/joeis/src/irvine/oeis/%parm%.java C:/Users/User/work/gits/joeis-lite/src/irvine/oeis/%parm%.java
+cp -v %GITS%/joeis/src/irvine/oeis/%parm%.java %GITS%/joeis-lite/src/irvine/oeis/%parm%.java
 echo #~ src/irvine/oeis/%parm%.java | sed -e "s/[ \r]//g" >> packlist.man
-uedit64 C:/Users/User/work/gits/joeis-lite/src/irvine/oeis/%parm%.java
-ls -al C:/Users/User/work/gits/joeis-lite/src/irvine/oeis/%parm%.java
+uedit64 %GITS%/joeis-lite/src/irvine/oeis/%parm%.java
+ls -al %GITS%/joeis-lite/src/irvine/oeis/%parm%.java
